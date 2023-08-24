2a3b010b5939ab1efbfee4366c2604eb67eb9a1d
git.publish01@dndmobile.co.kr <git.publish01@dndmobile.co.kr>
2023-08-24 14:57:21 +0900

    헤더 탭메뉴 수정

--- src/components/Common/Header.js
+++ src/components/Common/Header.js
@@ -7,6 +7,8 @@ const Header = () => {
   const router = useRouter();
   const pathname = router.pathname.substring(1);
   const [bar, setBar] = useState(pathname.startsWith("worksheet") ? 0 : pathname.startsWith("component") ? 1 : 2);
+  const [barWidth,setBarWidth] = useState('33.33%');
+  const [windowWidth, setWindowWidth] = useState();
   const [htmlClass, setHtmlClass] = useState('')
   const barRef = useRef(null);
   const { inputValue, setInputValue } = useSearchContext();
@@ -18,9 +20,9 @@ const Header = () => {
     if (bar === 0) {
       barRef.current.style.left = "5.328px";
     } else if (bar === 1) {
-      barRef.current.style.left = "101.328px";
+      barRef.current.style.left = "33%"
     } else {
-      barRef.current.style.left = "199.328px";
+      barRef.current.style.left = "66%"
     }
   }, [bar]);
 
@@ -33,12 +35,29 @@ const Header = () => {
     };
   },[htmlClass])
 
+  useEffect(() => {
+    setWindowWidth(window.innerWidth)
+    window.innerWidth < 768 ? setBarWidth('33.33%') : setBarWidth('95.3333px');
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth)
+      window.innerWidth < 768 ? setBarWidth('33.33%') : setBarWidth('95.3333px')
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <header id="pgHeader">
       <h1>
         <span className="hide">PUBLISHING WorkSheet</span>
       </h1>
-      <button type="button" className="btn_util">
+      <button type="button" className="btn_util" onClick={()=>{
+         document.documentElement.classList.contains("util_show") ?  document.documentElement.classList.remove('util_show') :  document.documentElement.classList.add('util_show')
+      }}>
         {/* <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="65px" height="50px" viewBox="0 0 65 65" xml:space="preserve">
           <path className="bar b1" d="M20,23.912h23.997c0,0,16.004-0.501,16.004,13.416 S46.75,47.912,41.834,42.995S22.668,23.412,22.668,23.412"></path>
           <path className="bar b2" d="M24.751,41.995H44c0,0,16.001,1.167,16.001-12.25 c0-13.417-9.508-15.157-18.004-6.745S22.001,42.995,22.001,42.995"></path>
@@ -57,13 +76,13 @@ const Header = () => {
           <span
             className="bar"
             style={{
-              width: "95.3333px",
+              width: `${barWidth}`,
               left: `${
                 pathname.startsWith("worksheet")
-                  ? "5.328px"
+                  ? "4px"
                   : pathname.startsWith("component")
-                  ? "101.328px"
-                  : "199.328px"
+                  ? "33.33%"
+                  : "66.66%"
               }`,
               opacity: "1",
               transform: "scale(1)",
@@ -82,7 +101,7 @@ const Header = () => {
               </Link>
             </li>
             <li
-              className={`${pathname.startsWith("component") ? "active" : ""}`}
+              className={`${pathname.startsWith("component") || pathname === '' ? "active" : ""}`}
               onMouseEnter={() => {
                 setBar(1);
               }}
